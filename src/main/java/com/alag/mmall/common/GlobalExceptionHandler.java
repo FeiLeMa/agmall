ae709a823626717c89d65cf2ab7dd2fc5dee6be3
alagMac <alan256@gmail.com>
2019-07-23 16:59:58 +0800

    添加全局异常处理GlobalExceptionHandle

---
+++ src/main/java/com/alag/mmall/common/GlobalExceptionHandler.java
@@ -0,0 +1,119 @@
+package com.alag.mmall.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.ConversionNotSupportedException;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+
+@ControllerAdvice
+@ResponseBody
+@Slf4j
+public class GlobalExceptionHandler {
+
+    private static final String logExceptionFormat = "Capture Exception By GlobalExceptionHandler: Code: {} Detail: {}";
+
+    //运行时异常
+    @ExceptionHandler(RuntimeException.class)
+    public ServerResponse runtimeExceptionHandler(RuntimeException ex) {
+        return resultFormat(1, ex);
+    }
+
+    //空指针异常
+    @ExceptionHandler(NullPointerException.class)
+    public ServerResponse nullPointerExceptionHandler(NullPointerException ex) {
+        return resultFormat(2, ex);
+    }
+
+    //类型转换异常
+    @ExceptionHandler(ClassCastException.class)
+    public ServerResponse classCastExceptionHandler(ClassCastException ex) {
+        return resultFormat(3, ex);
+    }
+
+    //IO异常
+    @ExceptionHandler(IOException.class)
+    public ServerResponse iOExceptionHandler(IOException ex) {
+        return resultFormat(4, ex);
+    }
+
+    //未知方法异常
+    @ExceptionHandler(NoSuchMethodException.class)
+    public ServerResponse noSuchMethodExceptionHandler(NoSuchMethodException ex) {
+        return resultFormat(5, ex);
+    }
+
+    //数组越界异常
+    @ExceptionHandler(IndexOutOfBoundsException.class)
+    public ServerResponse indexOutOfBoundsExceptionHandler(IndexOutOfBoundsException ex) {
+        return resultFormat(6, ex);
+    }
+
+    //400错误
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    public ServerResponse requestNotReadable(HttpMessageNotReadableException ex) {
+        System.out.println("400..requestNotReadable");
+        return resultFormat(7, ex);
+    }
+
+    //400错误
+    @ExceptionHandler({TypeMismatchException.class})
+    public ServerResponse requestTypeMismatch(TypeMismatchException ex) {
+        System.out.println("400..TypeMismatchException");
+        return resultFormat(8, ex);
+    }
+
+    //400错误
+    @ExceptionHandler({MissingServletRequestParameterException.class})
+    public ServerResponse requestMissingServletRequest(MissingServletRequestParameterException ex) {
+        System.out.println("400..MissingServletRequest");
+        return resultFormat(9, ex);
+    }
+
+    //405错误
+    @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
+    public ServerResponse request405(HttpRequestMethodNotSupportedException ex) {
+        return resultFormat(10, ex);
+    }
+
+    //406错误
+    @ExceptionHandler({HttpMediaTypeNotAcceptableException.class})
+    public ServerResponse request406(HttpMediaTypeNotAcceptableException ex) {
+        System.out.println("406...");
+        return resultFormat(11, ex);
+    }
+
+    //500错误
+    @ExceptionHandler({ConversionNotSupportedException.class, HttpMessageNotWritableException.class})
+    public ServerResponse server500(RuntimeException ex) {
+        System.out.println("500...");
+        return resultFormat(12, ex);
+    }
+
+    //栈溢出
+    @ExceptionHandler({StackOverflowError.class})
+    public ServerResponse requestStackOverflow(StackOverflowError ex) {
+        return resultFormat(13, ex);
+    }
+
+    //其他错误
+    @ExceptionHandler({Exception.class})
+    public ServerResponse exception(Exception ex) {
+        return resultFormat(14, ex);
+    }
+
+    private <T extends Throwable> ServerResponse resultFormat(Integer code, T ex) {
+        ex.printStackTrace();
+        log.error(logExceptionFormat,code,ex.getMessage());
+        return ServerResponse.createByErrorCodeMessage(code, ex.getMessage());
+    }
+
+}
